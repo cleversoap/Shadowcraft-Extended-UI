34ee2038ebd8f5ca156c988e132326d2bb4a564d
Matthew Clever <clever@cleversoap.com>
2011-01-21 12:32:57 +1000

    Initial commit. Base template.

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<h1>Shadowcraft</h1>
+		<table border="1">
+			<tr>
+				<td id="slot-0">
+					HEAD
+				</td>
+				<td id="core" rowspan="8" align="center" valign="top">
+					DATA
+					<table border="1">
+						<tr>
+							<td>
+								MAIN
+							</td>
+							<td>
+								OFF
+							</td>
+							<td>
+								RANGE
+							</td>
+						</tr>
+					</table>
+				</td>
+				<td id="slot-10">
+					HANDS
+				</td>
+			</tr>
+			<tr>
+				<td id="slot-1">
+					NECK
+				</td>
+				<td id="slot-2">
+					WAIST
+				</td>
+			</tr>
+			<tr>
+				<td id="slot-3">
+					SHOULDERS
+				</td>
+				<td id="slot-4">
+					LEGS
+				</td>
+			</tr>
+			<tr>
+				<td id="slot-5">
+					BACK
+				</td>
+				<td id="slot-6">
+					FEET
+				</td>
+			</tr>
+			<tr>
+				<td id="slot-7">
+					CHEST
+				</td>
+				<td id="slot-8">
+					RING
+				</td>
+			</tr>
+			<tr>
+				<td id="slot-9">
+					WRIST
+				</td>
+				<td id="slot-10">
+					RING
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td id="slot-11">
+					TRINKET
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td id="slot-12">
+					TRINKET
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
