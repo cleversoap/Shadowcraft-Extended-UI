8eff3b908576171cfdb8715e0ec24ed0d94b0a16
Matthew Clever <clever@cleversoap.com>
2011-02-03 14:19:54 +1000

    Base stats for races now complete. Some of the stats found on wowhead, like agility, for goblins and worgens were severely under the values of other races. Checking with the armory I added the correct values.

---
+++ races.js
@@ -0,0 +1,117 @@
+// This is a database of races and their base stats.
+// ALL VALUES ARE BASED ON UNGEARD, UNBUFFED, LEVEL 85
+// Stats were retrieved from wowhead & armory.
+// The base stats for goblins and worgens were off on wowhead by what I saw on the armory
+// eg. 196 agi instead of 218
+var races =
+{
+	goblin:
+	{
+		title:"Goblin",
+		stats:
+		{
+			str:119,
+			agi:218,
+			stam:114
+		}
+	},
+	
+	gnome:
+	{
+		title:"Gnome",
+		stats:
+		{
+			str:117,
+			agi:218,
+			stam:114
+		}
+	},
+	
+	night-elf:
+	{
+		title:"Night Elf",
+		stats:
+		{
+			str:118,
+			agi:220,
+			stam:114
+		}
+	},
+	
+	orc:
+	{
+		title:"Orc",
+		stats:
+		{
+			str:125,
+			agi:213,
+			stam:115
+		}
+	},
+	
+	blood-elf:
+	{
+		title:"Blood Elf",
+		stats:
+		{
+			str:119,
+			agi:218
+			stam:114
+		}
+	},
+	
+	human:
+	{
+		title:"Human",
+		stats:
+		{
+			str:122,
+			agi:216
+			stam:114
+		}
+	},
+	
+	dwarf:
+	{
+		title:"Dwarf",
+		stats:
+		{
+			str:127,
+			agi:212,
+			stam:115
+		}
+	},
+	
+	worgen:
+	{
+		title:"Worgen",
+		stats:
+		{
+			str:125,
+			agi:218,
+			stam:114
+		}
+	},
+	
+	troll:
+	{
+		title:"Troll",
+		stats:
+		{
+			str:123,
+			agi:218,
+			stam:114
+		}
+	},
+	
+	undead:
+	{
+		title:"Undead",
+		stats:
+		{
+			str:121,
+			agi:214,
+			stam:114
+		}
+	}
+}
